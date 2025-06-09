88ef613175be5525ae9445f34f3e00b9f536878f
Kazamapilot <kazamapilot@gmail.com>
2025-06-10 00:19:45 +0530

    what will events be lets see

--- src/Event/Event.hpp
+++ src/Event/Event.hpp
@@ -1,7 +1,24 @@
 #pragma once
-class Event
+#include<string>
+#include "EngineApi.hpp"
+class KNIGHT_ENGINE_API Event
 {
 public:
-
+	Event() = default;
+	virtual ~Event() = default;
+	// Event type enum
+	enum class Type {
+		None = 0,
+		WindowResize,
+		KeyPressed,
+		KeyReleased,
+		MouseButtonPressed,
+		MouseButtonReleased,
+		MouseMoved,
+		MouseScrolled
+	};
+	virtual Type GetType() const = 0;
+	virtual const char* GetName() const = 0;
+	virtual std::string ToString() const { return GetName(); }
 };
 
---
+++ src/Event/EventDispathcer.cpp
@@ -0,0 +1,1 @@
+#include "EventDispathcer.hpp"
