780cabb0c1b7e7d5045f8faa4c6a8e5a53c532b8
Kazamapilot <kazamapilot@gmail.com>
2025-06-26 16:00:19 +0530

    THINKING ABOUT ADDING PRECOMPILED HEADERS

---
+++ src/KnightEnginepch.cpp
@@ -0,0 +1,1 @@
+#include"KnightEnginepch.h"
---
+++ src/KnightEnginepch.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "EngineApi.hpp"
+#ifdef KNIGHT_PLATFORM_WINDOWS
+#include"Windows.h"
+#endif // 
+
+#include"vector"
+#include<iostream>
+
+#include "Core/Logger.hpp"
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+#include<map>
+#include<queue>
+#include<algorithm>
+#include"cstdint"
+#include<sstream>
+#include<functional>
+#include<memory>
+#include<utility>
+#include<stack>
+#include<memory>
