3f4529b7d87b8d96f9eb03e86e75c2c049a93812
Kazamapilot <kazamapilot@gmail.com>
2025-06-30 00:22:40 +0530

    LAYERS A NEW MONSTER

---
+++ src/Layer/Layer.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include"EngineApi.hpp"
+#include"Core/Logger.hpp"
+#include"Engine.hpp"
+#include"Event/Event.hpp"
+namespace KnightEngine {
+	class KNIGHT_ENGINE_API Layer
+	{
+	public:
+		Layer(const std::string& name = "Layer")
+			: m_DebugName(name) {
+			KE_TAG_LOG_INFO("Layer", "Layer created: {}", m_DebugName);
+		}
+		virtual ~Layer();
+
+		virtual void OnAttach() {
+
+		} ///< Called when the layer is attached to the application.
+
+		virtual void OnDetach() {
+		} ///< Called when the layer is detached from the application.
+		virtual void OnUpdate() {
+		} ///< Called every frame to update the layer.
+		virtual void OnEvent(Event&event) {
+
+		}
+		inline const std::string& GetName() const {
+			return m_DebugName;
+		} ///< Returns the name of the layer.
+	private:
+		std::string m_DebugName; ///< The name of the layer, used for debugging and logging.
+	};
+}
+
+
---
+++ src/Layer/LayerStack.hpp
@@ -0,0 +1,5 @@
+#pragma once
+class KNIGHT_ENGINE_API LayerStack
+{
+};
+
